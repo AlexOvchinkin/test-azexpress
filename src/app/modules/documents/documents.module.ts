2d65cd0e929fa6c6ad3f2c5f156749466adf658c
unknown <al.ovchinkin@gmail.com>
2018-09-13 19:21:39 +0300

    dist

--- src/app/modules/documents/documents.module.ts
+++ src/app/modules/documents/documents.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DocumentsGridComponent } from '../../components/documents-grid/documents-grid.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [
-    DocumentsGridComponent
-  ],
-  declarations: [DocumentsGridComponent]
+  exports: [],
+  declarations: []
 })
 export class DocumentsModule { }
